fcabedc0f3e4a18066ab8323bca6e3be2873d62f
Timothy Zhang <3147994+TimothyZhang023@users.noreply.github.com>
2018-03-26 16:14:09 +0800

    Update client.go

--- client/client.go
+++ client/client.go
@@ -10,6 +10,11 @@ import (
 	"strconv"
 
 	"github.com/imroc/req"
+	"sync"
+
+	_ "net/http/pprof"
+
+	"net/http"
 )
 
 type resp struct {
@@ -20,14 +25,14 @@ var partSize = 4 * 1024 * 1024
 var maxSplit = 4
 var server string
 
-func postContent(tid string, pid int, content string, c chan int) {
+func postContent(tid string, pid int, content []byte, c chan int) {
 	header := req.Header{
 		"Accept": "application/json",
 	}
-	param := req.Param{
-		"partContent": content,
-	}
-	_, err := req.Post(server+"/"+tid+"/part/"+strconv.Itoa(pid), header, param)
+	// param := req.Param{
+	// 	"partContent": string(content),
+	// }
+	_, err := req.Post(server+"/"+tid+"/part/"+strconv.Itoa(pid), header, content)
 	if err == nil {
 		// fmt.Println(r)
 	} else {
@@ -58,11 +63,16 @@ func getSize(path string) int64 {
 }
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	if len(os.Args) == 3 {
 		server = os.Args[1]
 		uploadFileName := os.Args[2]
 		f, e := os.Open(uploadFileName)
 		defer f.Close()
+
 		h := sha1.New()
 		if e == nil {
 			if _, err := io.Copy(h, f); err != nil {
@@ -85,34 +95,43 @@ func main() {
 					fmt.Println(r)
 					partLen := int(math.Ceil(float64(fileSize) / float64(partSize)))
 					fmt.Printf("total %d block\n", partLen)
-					buffer := make([]byte, partSize)
-					c := make(chan int)
-					splitCnt := 0
-					for i := 0; i < partLen; i++ {
-						len, err := f.ReadAt(buffer, int64(i*partSize))
-						if err != nil {
-							if len == 0 {
-								log.Fatalf("read file part %d error | %v", i, err)
-							} else {
-								splitCnt++
-								go postContent(fileSHA1, i, string(buffer[:len]), c)
-							}
-						} else {
-							splitCnt++
-							go postContent(fileSHA1, i, string(buffer[:len]), c)
-						}
-						fmt.Printf("process %d/%d\n", i, partLen)
-						if splitCnt >= maxSplit {
-							pid := <-c
-							splitCnt--
+					c := make(chan int, 10)
+					limit := make(chan int, 4)
+					go func() {
+						for pid := range c {
 							fmt.Printf("process %d done\n", pid)
 						}
+						fmt.Printf("all done\n")
+					}()
+
+					w := &sync.WaitGroup{}
+					for i := 0; i < partLen; i++ {
+						w.Add(1)
+						limit <- 1
+						go func(i int) {
+							defer w.Done()
+							// buffer := make([]byte, partSize)
+							buffer := BufferPool.Get().([]byte)
+							len, err := f.ReadAt(buffer, int64(i*partSize))
+							if err != nil {
+								if len == 0 {
+									log.Fatalf("read file part %d error | %v", i, err)
+								} else {
+									postContent(fileSHA1, i, (buffer[:len]), c)
+								}
+							} else {
+								postContent(fileSHA1, i, (buffer[:len]), c)
+							}
+
+							BufferPool.Put(buffer)
+							fmt.Printf("process %d/%d\n", i, partLen)
+							<-limit
+						}(i)
 					}
-					for splitCnt > 0 {
-						pid := <-c
-						splitCnt--
-						fmt.Printf("process %d done\n", pid)
-					}
+
+					w.Wait()
+					close(c)
+
 					done(fileSHA1)
 					fmt.Printf("upload done")
 				}
@@ -122,5 +141,15 @@ func main() {
 		}
 	} else {
 		log.Fatal("need file name")
+	}
+}
+
+var (
+	BufferPool sync.Pool
+)
+
+func init() {
+	BufferPool.New = func() interface{} {
+		return make([]byte, partSize)
 	}
 }
